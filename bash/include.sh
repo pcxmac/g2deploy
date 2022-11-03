936feb92ec9a61d5aa4d1c80f298a86bf9ef5d38
pxc <pxc@hypokrites.me>
2022-11-03 10:08:56 -0700

    pitstop

--- bash/include.sh
+++ bash/include.sh
@@ -7,8 +7,7 @@ function editboot()
 	local offset="$(getZFSMountPoint $DATASET)/boot/EFI/boot/refind.conf"
 	local POOL="${DATASET%/*}"
 	local UUID="$(blkid | grep "$POOL" | awk '{print $3}' | tr -d '"')"
-
-	line_number=$(grep -n "${DATASET} " ${offset}  | cut -f1 -d:)
+	local line_number=$(grep -n "${DATASET} " ${offset}  | cut -f1 -d:)
 	
 	sed -i "/default_selection/c default_selection $DATASET" ${offset}
 
@@ -37,13 +36,14 @@ function editboot()
 function clear_mounts()
 {
 	local offset=$1
+	local procs="$(lsof ${mountpoint} | sed '1d' | awk '{print $2}' | uniq)" 
+    local dir="$(echo "$offset" | sed -e 's/[^A-Za-z0-9\\/._-]/_/g')"
+	local output="$(cat /proc/mounts | grep "$dir" | wc -l)"
 
-	#procs="$(lsof ${mountpoint} | sed '1d' | awk '{print $2}' | uniq)" 
 	#echo "killing $(echo $procs | wc -l) process(s)"  2>&1
-	#for process in ${procs}; do kill -9 ${process}; done
+	for process in ${procs}; do kill -9 ${process}; done
 	#echo "umount $mountpoint"
 
-    dir="$(echo "$offset" | sed -e 's/[^A-Za-z0-9\\/._-]/_/g')"
 
 	if [[ -n "$(echo $dir | grep '/dev/')" ]]
 	then
@@ -51,9 +51,6 @@ function clear_mounts()
 	else
 		dir="${dir}\/"
 	fi
-
-	output="$(cat /proc/mounts | grep "$dir" | wc -l)"
-	echo "$output mounts to be removed" 
 
 	while [[ "$output" != 0 ]]
 	do
@@ -71,8 +68,7 @@ function mounts()
 {
     #echo "getting stage 3"
 	local offset=$1
-
-	mSize="$(cat /proc/meminfo | column -t | grep 'MemFree' | awk '{print $2}')"
+	local mSize="$(cat /proc/meminfo | column -t | grep 'MemFree' | awk '{print $2}')"
 	mSize="${mSize}K"
 
 	# MOUNTS
@@ -86,22 +82,20 @@ function mounts()
 	mount -t tmpfs -o size=$mSize tmpfs ${offset}/tmp
 	mount -t tmpfs tmpfs ${offset}/var/tmp
 	mount -t tmpfs tmpfs ${offset}/run
-
-
 	echo "attempting to mount binpkgs..."  2>&1
 	# this is to build in new packages for future installs, not always present
 	mount --bind /var/lib/portage/binpkgs ${offset}/var/lib/portage/binpkgs 
-	ls ${offset}/var/lib/portage/binpkgs
+	#ls ${offset}/var/lib/portage/binpkgs
 }
 
 function install_kernel()
 {
 	local offset=$1
-	kver="$(getKVER)"
-	kver="${kver#*linux-}"
-	ksrc="$(${SCRIPT_DIR}/bash/mirror.sh ${SCRIPT_DIR}/config/kernel.mirrors *)"
+	local kver="$(getKVER)"
+	local ksrc="$(${SCRIPT_DIR}/bash/mirror.sh ${SCRIPT_DIR}/config/kernel.mirrors *)"
+	local emergeOpts="--buildpkg=y --getbinpkg=y --binpkg-respect-use=y binpkg-changed-deps"
 
-	emergeOpts="--buildpkg=y --getbinpkg=y --binpkg-respect-use=y binpkg-changed-deps"
+	kver="${kver#*linux-}"
 
 	echo "${ksrc}${kver}/modules.tar.gz --output $offset/modules.tar.gz"
 	curl -L ${ksrc}${kver}/modules.tar.gz --output $offset/modules.tar.gz
@@ -115,14 +109,12 @@ function install_kernel()
 function mget()
 {
 
-
-	echo "$1 | $2" >&2
-
-
 	local url="$(echo "$1" | tr -d '*')"			# source_URL
 	local destination=$2	# destination_FS
+	local offset
+	local host
+	local _source
 
-	echo "${url} | ${destination}" >&2
 
 	case ${url%://*} in
 		# local rsync only
@@ -135,51 +127,33 @@ function mget()
 			mv ${destination}/${url#*://}* ${destination}/
 			url=${url#*://}
 			url=${url%%/*}
-			echo "${url}" 2>&1
+			#echo "${url}" 2>&1
 			rm ${destination}/${url} -R
 		;;
 
 		# local download only
 		ssh)
 			host=${url#*://}
-			source=${host#*:/}
+			_source=${host#*:/}
 			host=${host%:/*}
-			offset=$(echo "$source" | cut -d "/" -f1)
-
-			# ssh://root@10.1.0.1:/var/lib/portage/patchfiles/
-
-			ssh ${host} "tar cf - /${source}/" | pv --timer --rate | tar xf - -C ${destination}/
-
-			#ssh root@10.1.0.1 "tar cf - /var/lib/portage/patchfiles/" | pv --timer --rate | tar xf - -C /srv/zfs/wSys/systemd/var/lib/portage/patchfiles/
-
-			#destination="/srv/zfs/wSys/systemd"
-			#source="/var/lib/portage/patchfiles"
-			#offset="/var"
-
-			mv ${destination}/${source} ${destination}/__temp
-
+			offset=$(echo "$_source" | cut -d "/" -f1)
+			ssh ${host} "tar cf - /${_source}/" | pv --timer --rate | tar xf - -C ${destination}/
+			mv ${destination}/${_source} ${destination}/__temp
 			rm ${destination}/${offset} -R
 			mv ${destination}/__temp/* ${destination}
 			rm ${destination}/__temp -R
-
-			sleep 10
 		;;
 
 		# local file move only
 		file|*)
 			host=${url#*://}
-			source=${host#*:/}
+			_source=${host#*:/}
 			host=${host%:/*}
-
-			#echo "WTF ${url#*://}" 2>&1
 			if [[ ! -d "${url#*://}" ]] && [[ ! -f "${url#*://}" ]]; then exit; fi
 			if [[ ! -d "${destination}" ]]; then mkdir -p "${destination}"; fi
-
-			tar cf - /${source} | pv --timer --rate | tar xf - -C ${destination}/
-			mv ${destination}/${source} ${destination}/__temp
-			
-			offset=$(echo "$source" | cut -d "/" -f2)
-
+			tar cf - /${_source} | pv --timer --rate | tar xf - -C ${destination}/
+			mv ${destination}/${_source} ${destination}/__temp
+			offset=$(echo "$_source" | cut -d "/" -f2)
 			rm ${destination}/${offset} -R
 			mv ${destination}/__temp/* ${destination}
 			rm ${destination}/__temp -R
@@ -187,30 +161,22 @@ function mget()
 	esac
 	case ${url%://*} in
 		http|ftp|ssh|file|'')
-
 		;;
 	esac
 }
 
 function getKVER() 
 {
-
-	# coded for ftp accessable directory listing w/ curl and kernel.mirrors
-
-	url_kernel="$(${SCRIPT_DIR}/bash/mirror.sh ${SCRIPT_DIR}/config/kernel.mirrors *)"
-	kver="$(curl ${url_kernel} | sed -e 's/<[^>]*>//g' | awk '{print $9}' | \grep '.tar.gz$')"
+	local url_kernel="$(${SCRIPT_DIR}/bash/mirror.sh ${SCRIPT_DIR}/config/kernel.mirrors *)"
+	local kver="$(curl ${url_kernel} | sed -e 's/<[^>]*>//g' | awk '{print $9}' | \grep '.tar.gz$')"
 	kver=${kver%.tar.gz*}
 	echo ${kver}
-
 }
 
 function decompress() {
-
 	local src=$1
 	local dst=$2
-
 	local compression_type="$(file $src | awk '{print $2}')"
-	
 	case $compression_type in
 	'XZ')
 		pv $src | tar xJf - -C $dst
@@ -219,18 +185,14 @@ function decompress() {
 		pv $src | tar xzf - -C $dst
 		;;
 	esac
-
 }
 
 function getG2Profile() {
-
 	# assumes that .../amd64/17.X/... ; X will be preceeded by a decimal
-
 	local mountpoint=$1
 	local result="$(chroot $mountpoint /usr/bin/eselect profile show | tail -n1)"
 	result="${result#*.[0-9]/}"
 	echo $result
-
 }
 
 function getHostZPool () {
@@ -271,11 +233,15 @@ function zfs_keys()
 {
 	# ALL POOLS ON SYSTEM, FOR GENKERNEL
 	# pools="$(zpool list | awk '{print $1}') | sed '1 d')"
-	
 	# THE POOL BEING DEPLOYED TO ... -- DEPLOYMENT SCRIPT
 	#limit pools to just the deployed pool / not valid for genkernel which would attach all pools & datasets
 	local dataset=$1
 	local offset="$(zfs get mountpoint ${dataset} 2>&1 | sed -n 2p | awk '{print $3}')"
+	local dset
+	local format
+	local location
+	local location_type
+	local _source
 
 	local pools="$dataset"
 	pools="${pools%/*}"
@@ -309,17 +275,17 @@ function zfs_keys()
 				then
 					# if not, then probably https:/// ....
 					# put key file in to initramfs
-					source="${location#*//}"
-					destination="${source%/*}"
+					_source="${location#*//}"
+					destination="${_source%/*}"
 					destination="$offset$destination"
 					mkdir -p $destination
-					if test -f "$source"; then
-						#echo "copying $source to $destination"
-						cp $source $destination
+					if test -f "$_source"; then
+						#echo "copying $_source to $destination"
+						cp $_source $destination
 					#else
 						#echo "key not found for $j"
 					fi
-					#echo "coppied $source to $destination for $j"
+					#echo "coppied $_source to $destination for $j"
 				#else
 					#echo "nothing to do for $j ..."
 				fi
--- bash/update.sh
+++ bash/update.sh
@@ -18,11 +18,21 @@ SCRIPT_DIR="${SCRIPT_DIR%/*/${0##*/}*}"
 	#
 	#	future features : 	
 	#		test to see if pool exists, add new zfs datasets if no dataset, other partition types.
-	#		
+	#		NEED A STRUCTURE TO CHECK CURRENT BOOT SETTING/VERIFY MODULE INSTALL, THEN RETROFIT TO NEW KERNEL IF APPLICABLE, IF NOT, DONT DO SHIT TO THE KERNEL
 	#		
 	#	USE = ./update.sh work=pool/set boot=/dev/sdX update
 
 source ./include.sh
+
+function update_runtime() {
+
+	sudo emerge --sync --verbose --backtrack=99 --ask=n;sudo eix-update
+	sudo emerge -b -uDN --with-bdeps=y @world --ask=n --binpkg-respect-use=y --binpkg-changed-deps=y
+}
+
+export PYTHONPATH=""
+
+	export -f update_runtime
 
 # DESIGNATE A WORKING DIRECTORY TO 
 for x in $@
@@ -59,46 +69,11 @@ do
 			echo "PROFILE :: ${profile}"
 
 			patch_files ${directory} ${profile}
-
 			install_kernel ${directory}
+			editboot $(getKVER) ${dataset}
+			chroot ${directory} /bin/bash -c "update_runtime"
 
-			if [[ ! -f ./src/${latest_kernel}.tar.gz ]]
-			then
-				zcat /proc/config.gz > /usr/src/${latest_kernel}/.config
-				(cd /usr/src/${latest_kernel} ; make -j $(nproc))
-				(cd /usr/src/${latest_kernel} ; make modules_install)
-				(cd /usr/src/${latest_kernel} ; make install)
-				emerge =zfs-kmod-9999 =zfs-9999 --buildpkg=n --ask=n;
-				compress /usr/src/${latest_kernel} ./src/${latest_kernel}.tar.gz 
-				genkernel --install initramfs --compress-initramfs-type=lz4 --zfs
-
-				pathboot=/boot/LINUX/${latest_kernel#linux-*}
-				mkdir ${pathboot} -p
-
-				suffix=${latest_kernel##*-}
-
-				mv /boot/initramfs-${latest_kernel#linux-*}.img ${pathboot}/initramfs
-				mv /boot/vmlinuz-${latest_kernel#linux-*} ${pathboot}/vmlinuz
-				mv /boot/System.map-${latest_kernel#linux-*} ${pathboot}/System.map-${latest_kernel#linux-*}
-				mv /boot/config-${latest_kernel#linux-*} ${pathboot}/config-${latest_kernel#linux-*}
-			fi
-
-
-
-			echo "editing boot record"
-			
-			editboot ${latest_kernel#linux-*} ${directory}
-
-			echo "syncing kernel modules for ${latest_kernel#linux-*}"
-			
-			rsync -c -a -r -l -H -p --delete-before --info=progress2 /lib/modules/${latest_kernel#linux-*} $directory/lib/modules/
-
-			echo "@ ${directory}"
-			config_env ${directory}
-			chroot ${directory} /usr/bin/emerge --sync
-			chroot ${directory} /usr/bin/emerge -b -uDN @world $emergeOpts
-
-			check_mounts ${directory}
+			clear_mounts ${directory}
 
 
 		;;
