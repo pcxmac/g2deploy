ef4c42219306193efd4a0fabf3ab65ac8b6100d7
pxc <pxc@hypokrites.me>
2022-12-03 20:23:17 -0800

    meta package manager

--- bash/mpm.sh
+++ bash/mpm.sh
@@ -2,7 +2,8 @@
 SCRIPT_DIR="$(realpath ${BASH_SOURCE:-$0})"
 SCRIPT_DIR="${SCRIPT_DIR%/*/${0##*/}*}"
 
-    #   INPUTS    
+    #           meta package manager (MPM)
+    #   INPUTS 
 	#   
 	#	    package=<bundle_manifest> ... reference to manifest file, tar's all the contents listed in the manifest by -L
     #       install=<bundle_pkg> ... access /bundle/.../<bundle_pkg>, executes script, then installs patchfiles
