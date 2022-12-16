0574e402b844bee3b521179d5fcddf7dfd16b10c
pcxmac <ciaran@hypokrites.me>
2022-12-16 12:57:10 -0800

    master password is password

--- bash/deploy.sh
+++ bash/deploy.sh
@@ -37,10 +37,10 @@ function users()
 	#	
 
 	usermod -s /bin/zsh root
-	sudo sh -c 'echo root:@PCXmacR00t | chpasswd' 2>/dev/null
+	sudo sh -c 'echo root:password | chpasswd' 2>/dev/null
 	# CYCLE THROUGH USERS ?
 	useradd sysop
-	sudo sh -c 'echo sysop:@PCXmacSy$ | chpasswd' 2>/dev/null
+	sudo sh -c 'echo sysop:password | chpasswd' 2>/dev/null
 	echo "home : sysop"
 	usermod --home /home/sysop sysop
 	echo "wheel : sysop"
