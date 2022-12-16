ee76d8052cec4d210c0b67fca2aa280374808d60
pcxmac <ciaran@hypokrites.me>
2022-12-16 03:12:27 -0800

    some distfiles servers have bad modify times

--- bash/esync.sh
+++ bash/esync.sh
@@ -23,19 +23,19 @@ emerge --sync | tee /var/log/esync.log
 echo "############################### [ SNAPSHOTS ] ###################################"
 URL="$(${SCRIPT_DIR}/bash/mirror.sh ${SCRIPT_DIR}/config/snapshots.mirrors rsync)"
 echo -e "SYNCING w/ $URL \e[25,42m[SNAPSHOTS]\e[0m";sleep 1
-rsync -avI --links --info=progress2 --timeout=300 --no-perms --ignore-existing --no-owner --no-group ${URL} /var/lib/portage/ | tee /var/log/esync.log
+rsync -avI --links --info=progress2 --timeout=300 --no-perms --ignore-times --ignore-existing --no-owner --no-group ${URL} /var/lib/portage/ | tee /var/log/esync.log
 
 # THIRD ARG only-sync is a trivial argument, the profile inside mirror.sh will not recognize this type of profile(only-sync), and will throw a null value for release_base_string
 echo "############################### [ RELEASES ] ###################################"
 URL="$(${SCRIPT_DIR}/bash/mirror.sh ${SCRIPT_DIR}/config/releases.mirrors rsync only-sync)"
 echo -e "SYNCING w/ $URL \e[25,42m[RELEASES]\e[0m";sleep 1
 if [[ ! -d /var/lib/portage/releases ]]; then mkdir -p /var/lib/portage/releases; fi
-rsync -avI --links --info=progress2 --timeout=300 --no-perms --ignore-existing --no-owner --no-group ${URL}${ARCH} /var/lib/portage/releases | tee /var/log/esync.log
+rsync -avI --links --info=progress2 --timeout=300 --no-perms --ignore-times --ignore-existing --no-owner --no-group ${URL}${ARCH} /var/lib/portage/releases | tee /var/log/esync.log
 
 echo "############################### [ DISTFILES ] ###################################"
 URL="$(${SCRIPT_DIR}/bash/mirror.sh ${SCRIPT_DIR}/config/distfiles.mirrors rsync)"
 echo -e "SYNCING w/ $URL \e[25,42m[DISTFILES]\e[0m";sleep 1
-rsync -avI --info=progress2 --timeout=300 --ignore-existing --no-perms --no-owner --no-group ${URL} /var/lib/portage/ | tee /var/log/esync.log
+rsync -avI --info=progress2 --timeout=300 --ignore-existing --ignore-times --no-perms --no-owner --no-group ${URL} /var/lib/portage/ | tee /var/log/esync.log
 
 echo "updating mlocate-db"
 /usr/bin/updatedb
