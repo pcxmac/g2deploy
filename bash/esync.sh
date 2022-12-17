c3fa2cd585274f345e67caf49f56c8386499a443
pcxmac <ciaran@hypokrites.me>
2022-12-17 00:14:19 -0800

    test

--- bash/esync.sh
+++ bash/esync.sh
@@ -13,8 +13,6 @@ ARCH=""             # all archetectures
 /sbin/rc-service lighttpd stop
 /bin/sync
 
-
-
 echo "################################# [ REPOS ] #####################################"
 URL="rsync://rsync.us.gentoo.org/gentoo-portage/"
 echo -e "SYNCING w/ ***$URL*** [REPOS]"
@@ -25,7 +23,6 @@ URL="$(${SCRIPT_DIR}/bash/mirror.sh ${SCRIPT_DIR}/config/snapshots.mirrors rsync
 echo -e "SYNCING w/ $URL \e[25,42m[SNAPSHOTS]\e[0m";sleep 1
 rsync -avI --links --info=progress2 --timeout=300 --no-perms --ignore-times --ignore-existing --no-owner --no-group ${URL} /var/lib/portage/ | tee /var/log/esync.log
 
-# THIRD ARG only-sync is a trivial argument, the profile inside mirror.sh will not recognize this type of profile(only-sync), and will throw a null value for release_base_string
 echo "############################### [ RELEASES ] ###################################"
 URL="$(${SCRIPT_DIR}/bash/mirror.sh ${SCRIPT_DIR}/config/releases.mirrors rsync only-sync)"
 echo -e "SYNCING w/ $URL \e[25,42m[RELEASES]\e[0m";sleep 1
@@ -42,9 +39,7 @@ echo "updating mlocate-db"
 /usr/bin/eix-update
 
 hostip="$(/bin/route -n | /bin/grep "^0.0.0.0" | /usr/bin/awk '{print $8}')"
-#echo "$hostip"
 hostip="$(/bin/ip --brief address show dev $hostip | /usr/bin/awk '{print $3}')"
-#echo "$hostip"
 
 /bin/sed -i "s|   HOST:.*|   HOST: ${hostip}|" /etc/rsync/rsyncd.motd
 /bin/sed -i "s|   DATE:.*|   DATE: $(date)|" /etc/rsync/rsyncd.motd
