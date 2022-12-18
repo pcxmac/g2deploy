8f248eed26198535f0ed9fb856a00e8237456aad
pcxmac <ciaran@hypokrites.me>
2022-12-17 23:16:39 -0800

    sync commit

--- bash/syncMeta.sh
+++ bash/syncMeta.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(realpath ${BASH_SOURCE:-$0})"
 SCRIPT_DIR="${SCRIPT_DIR%/*/${0##*/}*}"
 
 source ./include.sh
 
-mget /var/lib/portage/meta	    ${SCRIPT_DIR}/meta
-mget /var/lib/portage/profiles	${SCRIPT_DIR}/profiles
-mget /var/lib/portage/packages	${SCRIPT_DIR}/packages
+echo "script dir = ${SCRIPT_DIR}"
+
+#
+#
+#	fix the destination, last slash, and replicating the same directory in itself
+#
+#
+
+mget rsync://10.1.0.1/gentoo/meta/*			${SCRIPT_DIR}/meta/
+mget rsync://10.1.0.1/gentoo/profiles/*		${SCRIPT_DIR}/profiles/
+mget rsync://10.1.0.1/gentoo/packages/*		${SCRIPT_DIR}/packages/
+mget rsync://10.1.0.1/gentoo/patchfiles/*	${SCRIPT_DIR}/patchfiles/
+
+owner="$(stat -c '%U' ${SCRIPT_DIR})"
+group="$(stat -c '%G' ${SCRIPT_DIR})"
+
+chown ${owner}:${group} ${SCRIPT_DIR}/meta -R			1>/dev/null
+chown ${owner}:${group} ${SCRIPT_DIR}/profiles -R		1>/dev/null
+chown ${owner}:${group} ${SCRIPT_DIR}/packages -R		1>/dev/null
+chown ${owner}:${group} ${SCRIPT_DIR}/patchfiles -R		1>/dev/null
