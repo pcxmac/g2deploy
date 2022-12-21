4e33af02080053c3d29e0c09aab0964e82d143c6
pcxmac <ciaran@hypokrites.me>
2022-12-21 02:32:28 -0800

    new tid bits

---
+++ zmisc/git_gpg_key.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# see if one exists ?
+#gpg --list-secret-keys --keyid-format=long
+
+#sleep 5
+
+#gpg --delete-secret-key ${name}
+
+#gpg --full-generate-key
+
+#gpg --armor --export ${public_key}
+
+#git config --global user.signingkey ${key}
---
+++ zmisc/git_ssh_key.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+#ssh-keygen -t ed25519 -C "ciaran@hypokrites.me"
+ssh-keygen -t rsa -b 4096 -C "ciaran@hypokrites.me"
+
+# add to .ssh/config
+
+# delete authorized keys
+
