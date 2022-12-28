cf9a07e11103e79d66e3a9bb537424cbe388a23d
git <ciaran@hypokrites.me>
2022-12-28 04:07:23 -0800

    enrolling host.cfg / scanConfig, for dynamic servs

--- bash/sync.sh
+++ bash/sync.sh
@@ -21,6 +21,9 @@ SCRIPT_DIR="${SCRIPT_DIR%/*/${0##*/}*}"
 
 source ${SCRIPT_DIR}/bash/include.sh
 
+pkgHOST="$(scanConfig ${SCRIPT_DIR}/config/host.cfg pkgserver host)"
+pkgROOT="$(scanConfig ${SCRIPT_DIR}/config/host.cfg pkgserver root)"
+
 echo "################################# [ REPOS ] #####################################"
 URL="rsync://rsync.us.gentoo.org/gentoo-portage/"
 echo -e "SYNCING w/ ***$URL*** [REPOS]"
@@ -43,19 +46,19 @@ emerge --sync | tee /var/log/esync.log
 echo "############################### [ SNAPSHOTS ] ###################################"
 URL="$(${SCRIPT_DIR}/bash/mirror.sh "${SCRIPT_DIR}/config/snapshots.mirrors" rsync)"
 echo -e "SYNCING w/ $URL \e[25,42m[SNAPSHOTS]\e[0m";sleep 1
-rsync -avI --links --info=progress2 --timeout=300 --no-perms --ignore-times --ignore-existing --no-owner --no-group "${URL}" /var/lib/portage/ | tee /var/log/esync.log
+rsync -avI --links --info=progress2 --timeout=300 --no-perms --ignore-times --ignore-existing --no-owner --no-group "${URL}" "${pkgROOT}"/ | tee /var/log/esync.log
 
 echo "############################### [ RELEASES ] ###################################"
 URL="$(${SCRIPT_DIR}/bash/mirror.sh "${SCRIPT_DIR}/config/releases.mirrors" rsync only-sync)"
 echo -e "SYNCING w/ $URL \e[25,42m[RELEASES]\e[0m";sleep 1
-if [[ ! -d /var/lib/portage/releases ]]; then mkdir -p /var/lib/portage/releases; fi
-find /var/lib/portage/releases/ -type l -delete
-rsync -avI --links --info=progress2 --timeout=300 --no-perms --ignore-times --ignore-existing --no-owner --no-group "${URL}${ARCH}" /var/lib/portage/releases | tee /var/log/esync.log
+if [[ ! -d "${pkgROOT}"/releases ]]; then mkdir -p "${pkgROOT}"/releases; fi
+find "${pkgROOT}"/releases/ -type l -delete
+rsync -avI --links --info=progress2 --timeout=300 --no-perms --ignore-times --ignore-existing --no-owner --no-group "${URL}${ARCH}" "${pkgROOT}"/releases | tee /var/log/esync.log
 
 echo "############################### [ DISTFILES ] ###################################"
 URL="$(${SCRIPT_DIR}/bash/mirror.sh "${SCRIPT_DIR}/config/distfiles.mirrors" rsync)"
 echo -e "SYNCING w/ $URL \e[25,42m[DISTFILES]\e[0m";sleep 1
-rsync -avI --info=progress2 --timeout=300 --ignore-existing --ignore-times --no-perms --no-owner --no-group "${URL}" /var/lib/portage/ | tee /var/log/esync.log
+rsync -avI --info=progress2 --timeout=300 --ignore-existing --ignore-times --no-perms --no-owner --no-group "${URL}" "${pkgROOT}"/ | tee /var/log/esync.log
 
 echo "updating mlocate-db"
 /usr/bin/updatedb
@@ -64,20 +67,17 @@ echo "updating mlocate-db"
 hostip="$(/bin/route -n | /bin/grep "^0.0.0.0" | /usr/bin/awk '{print $8}')"
 hostip="$(/bin/ip --brief address show dev ${hostip} | /usr/bin/awk '{print $3}')"
 
-pkgHOST="$(scanConfig ${SCRIPT_DIR}/config/host.cfg pkgserver host)"
-
 mget "rsync://${pkgHOST}/gentoo/meta/*"		    "${SCRIPT_DIR}/meta/"
 mget "rsync://${pkgHOST}/gentoo/profiles/*"	    "${SCRIPT_DIR}/profiles/"
 mget "rsync://${pkgHOST}/gentoo/packages/*"	    "${SCRIPT_DIR}/packages/"
 mget "rsync://${pkgHOST}/gentoo/patchfiles/*"	"${SCRIPT_DIR}/patchfiles/"
 
-owner="$(stat -c '%U' "${SCRIPT_DIR}")"
+wner="$(stat -c '%U' "${SCRIPT_DIR}")"
 group="$(stat -c '%G' "${SCRIPT_DIR}")"
 
 chown "${owner}:${group}" "${SCRIPT_DIR}/meta" -R			1>/dev/null
 chown "${owner}:${group}" "${SCRIPT_DIR}/profiles" -R		1>/dev/null
 chown "${owner}:${group}" "${SCRIPT_DIR}/packages" -R		1>/dev/null
-
 
 repoServer="https://gitweb.gentoo.org/repo/gentoo.git/"
 
