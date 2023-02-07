a01b3701043e68f3c25da806845ed850507fea08
ciaran <ciaran@hypokrites.me>
2023-02-07 04:08:03 -0800

    move to include

--- bash/deploy.sh
+++ bash/deploy.sh
@@ -5,142 +5,6 @@ SCRIPT_DIR="${SCRIPT_DIR%/*/${0##*/}*}"
 
 source ${SCRIPT_DIR}/bash/include.sh
 
-function users()
-{
-	usermod -s /bin/zsh root
-	sudo sh -c 'echo root:@PXCW0rd | chpasswd' 2>/dev/null
-	useradd sysop
-	sudo sh -c 'echo sysop:@PXCW0rd | chpasswd' 2>/dev/null
-	usermod --home /home/sysop sysop
-	usermod -a -G wheel,portage sysop
-	usermod --shell /bin/zsh sysop
-	homedir="$(eval echo ~sysop)"
-	chown sysop:sysop "${homedir}" -R 2>/dev/null
-}
-
-function buildup()
-{
-
-	local offset="${2:?}"
-	local selection="${4:?}"
-
-	count="$(find "${offset}/" | wc -l)"
-
-	if [[ ${count} -gt 1 ]]
-	then
-		rm -rv ${offset:?}/* | pv -l -s "${count}" > /dev/null
-	else
-		echo -e "done "
-	fi
-
-	files="$(${SCRIPT_DIR}/bash/mirror.sh "${SCRIPT_DIR}/config/mirrors/releases" http "${selection}")"
-	filexz="$(echo "${files}" | grep '.xz$')"
-	fileasc="$(echo "${files}" | grep '.asc$')"
-	serverType="${filexz%//*}"
-
-	case ${serverType%//*} in
-		"file:/")
-			mget "${filexz#*//}" "${offset}/"
-			mget "${fileasc#*//}" "${offset}/"
-		;;
-		"http:"|"rsync:")
-			mget "${filexz}" "${offset}/"
-			mget "${fileasc}" "${offset}/"
-		;;
-	esac
-
-	fileasc="${fileasc##*/}"
-	filexz="${filexz##*/}"
-
-	gpg --verify "${offset}/${fileasc}"
-	rm ${offset}/${fileasc}
-
-	decompress "${offset}/${filexz}" "${offset}"
-	rm ${offset}/${filexz}
-
-	mkdir -p "${offset}/var/lib/portage/binpkgs"
-	mkdir -p "${offset}/var/lib/portage/distfiles"
-	mkdir -p "${offset}/srv/crypto/"
-	mkdir -p "${offset}/var/lib/portage/repos/gentoo"
-}
-
-function system()
-{
-	local emergeOpts="--buildpkg=y --getbinpkg=y --binpkg-respect-use=y --binpkg-changed-deps=y --backtrack=99 --verbose --tree --verbose-conflicts"
-
-	pv="$(qlist -Iv | \grep 'sys-apps/portage' | \grep -v '9999' | head -n 1)"
-	av="$(pquery sys-apps/portage --max 2>/dev/null)"
-	echo "DEPLOY::CHECKING PORTAGE ${av##*-}/${pv##*-}"
-
-	if [[ "${av##*-}" != "${pv##*-}" ]]
-	then 
-		emerge portage --oneshot --ask=n
-	fi
-
-	echo "DEPLOY::ISSUING UPDATES"
-	emerge ${emergeOpts} -b -uDN --with-bdeps=y @world --ask=n
-
-	echo "APPLYING NECCESSARY PRE-BUILD PATCHES"
-	
-	sh < /patches.sh
-
-	#rm /patches.sh
-
-	echo "DEPLOY::EMERGE PROFILE PACKAGES"
-	emerge ${emergeOpts} $(cat /package.list)
-	#rm /package.list
-
-	echo "DEPLOY::EMERGE ZED FILE SYSTEM"
-	emergeOpts="--verbose-conflicts"
-	FEATURES="-getbinpkg -buildpkg"
-	emerge ${emergeOpts} =zfs-9999 --nodeps
-
-	local emergeOpts="--buildpkg=y --getbinpkg=y --binpkg-respect-use=y --binpkg-changed-deps=y --backtrack=99 --verbose --tree --verbose-conflicts"
-	echo "DEPLOY::POST INSTALL UPDATE !!!"
-	emerge -b -uDN --with-bdeps=y @world --ask=n ${emergeOpts}
-
-	wget -O - https://qa-reports.gentoo.org/output/service-keys.gpg | gpg --import
-
-	eselect news read new
-
-	eix-update
-	updatedb
-}
-
-function services() 
-{
-	echo "DEPLOY::EXECUTING SERVICE ROUTINE"
-	sh < /services.sh
-	rm /services.sh
-}
-
-#function services()
-#{
-#	local service_list="${1:?}"
-#	bash <(curl "${service_list}" --silent)
-#}
-
-function locales()
-{
-
-    local key="${1:?}"
-	locale-gen -A
-	eselect locale set en_US.utf8
-
-	emerge-webrsync
-	emerge --sync --ask=n
-
-	eselect profile set default/linux/amd64/${key%/openrc}
-	eselect profile show
-	sleep 2
-
-    echo "reading the news (null)..."
-	eselect news read all > /dev/null
-	echo "America/Los_Angeles" > /etc/timezone
-	emerge --config sys-libs/timezone-data
-	
-}
-
 	export PYTHONPATH=""
 
 	export -f users
@@ -198,8 +62,11 @@ function locales()
 	mounts "${directory}"
 
 	patchFiles_user "${directory}" "${_profile}"
-	patchFiles_sys "${directory}" "${_profile}"
 	patchFiles_portage "${directory}" "${_profile}"
+
+	ls ${directory}/usr/lib64/* -ail
+
+	sleep 100
 
 	zfs_keys "${dataset}"
 
@@ -209,10 +76,11 @@ function locales()
 	patchSystem "${_profile}" 'services' > "${directory}/services.sh"
 
 	chroot "${directory}" /bin/bash -c "locales ${_profile}"
-
  	chroot "${directory}" /bin/bash -c "system"
-	chroot "${directory}" /bin/bash -c "users ${_profile}"
 
+	patchFiles_sys "${directory}" "${_profile}"
+
+	chroot "${directory}" /bin/bash -c "users ${_profile}"
 	chroot "${directory}" /bin/bash -c "services"
 
 	#services_URL="$(echo "$(${SCRIPT_DIR}/bash/mirror.sh "${SCRIPT_DIR}/config/mirrors/package" http)/${_profile}.services" | sed 's/ //g' | sed "s/\"/'/g")"
--- bash/include.sh
+++ bash/include.sh
@@ -6,6 +6,142 @@ SCRIPT_DIR="${SCRIPT_DIR%/*}"
 source ${SCRIPT_DIR}/bash/mget.sh
 source ${SCRIPT_DIR}/bash/yaml.sh
 
+function users()
+{
+	usermod -s /bin/zsh root
+	sudo sh -c 'echo root:@PXCW0rd | chpasswd' 2>/dev/null
+	useradd sysop
+	sudo sh -c 'echo sysop:@PXCW0rd | chpasswd' 2>/dev/null
+	usermod --home /home/sysop sysop
+	usermod -a -G wheel,portage sysop
+	usermod --shell /bin/zsh sysop
+	homedir="$(eval echo ~sysop)"
+	chown sysop:sysop "${homedir}" -R 2>/dev/null
+}
+
+function buildup()
+{
+
+	local offset="${2:?}"
+	local selection="${4:?}"
+
+	count="$(find "${offset}/" | wc -l)"
+
+	if [[ ${count} -gt 1 ]]
+	then
+		rm -rv ${offset:?}/* | pv -l -s "${count}" > /dev/null
+	else
+		echo -e "done "
+	fi
+
+	files="$(${SCRIPT_DIR}/bash/mirror.sh "${SCRIPT_DIR}/config/mirrors/releases" http "${selection}")"
+	filexz="$(echo "${files}" | grep '.xz$')"
+	fileasc="$(echo "${files}" | grep '.asc$')"
+	serverType="${filexz%//*}"
+
+	case ${serverType%//*} in
+		"file:/")
+			mget "${filexz#*//}" "${offset}/"
+			mget "${fileasc#*//}" "${offset}/"
+		;;
+		"http:"|"rsync:")
+			mget "${filexz}" "${offset}/"
+			mget "${fileasc}" "${offset}/"
+		;;
+	esac
+
+	fileasc="${fileasc##*/}"
+	filexz="${filexz##*/}"
+
+	gpg --verify "${offset}/${fileasc}"
+	rm ${offset}/${fileasc}
+
+	decompress "${offset}/${filexz}" "${offset}"
+	rm ${offset}/${filexz}
+
+	mkdir -p "${offset}/var/lib/portage/binpkgs"
+	mkdir -p "${offset}/var/lib/portage/distfiles"
+	mkdir -p "${offset}/srv/crypto/"
+	mkdir -p "${offset}/var/lib/portage/repos/gentoo"
+}
+
+function system()
+{
+	local emergeOpts="--buildpkg=y --getbinpkg=y --binpkg-respect-use=y --binpkg-changed-deps=y --backtrack=99 --verbose --tree --verbose-conflicts"
+
+	pv="$(qlist -Iv | \grep 'sys-apps/portage' | \grep -v '9999' | head -n 1)"
+	av="$(pquery sys-apps/portage --max 2>/dev/null)"
+	echo "DEPLOY::CHECKING PORTAGE ${av##*-}/${pv##*-}"
+
+	if [[ "${av##*-}" != "${pv##*-}" ]]
+	then 
+		emerge portage --oneshot --ask=n
+	fi
+
+	echo "DEPLOY::ISSUING UPDATES"
+	emerge ${emergeOpts} -b -uDN --with-bdeps=y @world --ask=n
+
+	echo "APPLYING NECCESSARY PRE-BUILD PATCHES"
+	
+	sh < /patches.sh
+
+	#rm /patches.sh
+
+	echo "DEPLOY::EMERGE PROFILE PACKAGES"
+	emerge ${emergeOpts} $(cat /package.list)
+	#rm /package.list
+
+	echo "DEPLOY::EMERGE ZED FILE SYSTEM"
+	emergeOpts="--verbose-conflicts"
+	FEATURES="-getbinpkg -buildpkg"
+	emerge ${emergeOpts} =zfs-9999 --nodeps
+
+	local emergeOpts="--buildpkg=y --getbinpkg=y --binpkg-respect-use=y --binpkg-changed-deps=y --backtrack=99 --verbose --tree --verbose-conflicts"
+	echo "DEPLOY::POST INSTALL UPDATE !!!"
+	emerge -b -uDN --with-bdeps=y @world --ask=n ${emergeOpts}
+
+	wget -O - https://qa-reports.gentoo.org/output/service-keys.gpg | gpg --import
+
+	eselect news read new
+
+	eix-update
+	updatedb
+}
+
+function services() 
+{
+	echo "DEPLOY::EXECUTING SERVICE ROUTINE"
+	sh < /services.sh
+	rm /services.sh
+}
+
+#function services()
+#{
+#	local service_list="${1:?}"
+#	bash <(curl "${service_list}" --silent)
+#}
+
+function locales()
+{
+
+    local key="${1:?}"
+	locale-gen -A
+	eselect locale set en_US.utf8
+
+	emerge-webrsync
+	emerge --sync --ask=n
+
+	eselect profile set default/linux/amd64/${key%/openrc}
+	eselect profile show
+	sleep 2
+
+    echo "reading the news (null)..."
+	eselect news read all > /dev/null
+	echo "America/Los_Angeles" > /etc/timezone
+	emerge --config sys-libs/timezone-data
+	
+}
+
 function tStamp() 
 {
 	echo "0x$(echo "obase=16; $(date +%s)" | bc)"
